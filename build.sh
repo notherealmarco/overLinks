65caec6dd441161f9e5ba57812e108794078738f
Marco Realacci <marco@marcorealacci.me>
2023-01-26 16:10:03 +0100

    Fix script not always pulling by workaround

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 cd /mnt/cache/SSD/overLinks/overLinks
+git pull origin main --recurse-submodules
+cd frontend
+git pull origin main
+cd ..
+cd telegram
+git pull origin main
+cd ..
 docker build -t overlinks-be -f Dockerfile.backend .
 docker build -t overlinks-fe -f Dockerfile.frontend .
 docker build -t overlinks-tg -f Dockerfile.telegram .
