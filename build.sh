4c794bd1aca5221d3fd75124deb37754a7640501
Marco Realacci <marco@marcorealacci.me>
2023-01-26 15:49:10 +0100

    Fix build script arguments

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 cd /mnt/cache/SSD/overLinks/overLinks
-docker build -t overlinks-be Dockerfile.backend .
-docker build -t overlinks-fe Dockerfile.frontend .
-docker build -t overlinks-tg Dockerfile.telegram .
+docker build -t overlinks-be -f Dockerfile.backend .
+docker build -t overlinks-fe -f Dockerfile.frontend .
+docker build -t overlinks-tg -f Dockerfile.telegram .
